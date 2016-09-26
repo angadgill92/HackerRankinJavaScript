37b963bfbd8cefed0f3243d1edd8db2e69e04817
Angad Gill <angadpalsingh92@gmail.com>
2016-09-26 17:49:35 +0530

    insertion sort [updated]

--- InsertionSortOne.js
+++ InsertionSortOne.js
@@ -1,25 +1,21 @@
-function insert (array) { // takes an array as input and prints steps of insertion sort
-    let tempArr = array
-    let last = array[array.length-1]
-    let index = tempArr.length - 2
+const insert = (array) => { // takes an array as input and prints steps of insertion sort
+  let tempArr = array
+  let last = array[array.length - 1]
+  let index = tempArr.length - 2
 
-    while (index >= -1 ) {
-       if (tempArr[index] > last){
-            tempArr[index + 1] = tempArr[index]
-            console.log(tempArr.join(' '))
-        } else if (tempArr[index] <= last ){
-
-           tempArr[index + 1] = last
-           console.log(tempArr.join(' '))
-           break
-
-        } else {
-            tempArr[0] = last
-            console.log(tempArr.join(' '))
-            break
-
-        }
-        index--
+  while (index >= -1) {
+    if (tempArr[index] > last) {
+      tempArr[index + 1] = tempArr[index]
+      console.log(tempArr.join(' '))
+    } else if (tempArr[index] <= last) {
+      tempArr[index + 1] = last
+      console.log(tempArr.join(' '))
+      break
+    } else {
+      tempArr[0] = last
+      console.log(tempArr.join(' '))
+      break
     }
-
+    index--
+  }
 }
